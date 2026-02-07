c2a1b6028a8cac83a331459bca052e3fca7962c2
Aloysius <aloysius.tan00@gmail.com>
2026-02-07 18:55:17 +0800

    Lab Exercise

---
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,13 @@
+import './WeatherForecast.css'
+
+
+export const WeatherForecast = ({day, img, imgAlt, conditions, time}) => {
+    return (
+        <div className="weather">
+            <h2>{day}</h2>
+            <img src={img} alt={imgAlt} />
+            <p><span>conditions: </span>{conditions}</p>
+            <p><span>time: </span>{time}</p>
+        </div>
+    );
+}
